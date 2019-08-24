58b285736eb7e14509fe308d21493a4abf0e6b63
naknakun <whskrdns@gmail.com>
2019-08-24 11:32:14 +0900

    fixed untracked files

--- web/VRServer/routes/index.js
+++ web/VRServer/routes/index.js
@@ -52,4 +52,14 @@ router.get('/someapi', function(req, res){
   });  
 });
 
+router.post('/SendText', function(req, res){
+  console.log(req.body.id + ', ' + req.body.text);
+  if (req.body.id && req.body.text){
+    res.send('제대로 전송되었습니다.');
+  }
+  else{
+    res.send('받은 데이터가 읍어요');
+  }
+ });
+ 
 module.exports = router;
